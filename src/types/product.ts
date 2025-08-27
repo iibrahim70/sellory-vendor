b13a5d81aba8a1003da14945ec8daebca3c74f51
iibrahim70 <iibrahiim.dev@gmail.com>
2025-08-27 22:18:23 +0600

    :art: add Product interface to define product structure

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+  discount: number;
+  discountPrice: number;
+  rating: number;
+  totalRating: number;
+  endTime: string;
+}
